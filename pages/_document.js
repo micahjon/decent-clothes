4820fffa0628c1ca87fd274d2df7d4b6d9c2f16b
Micah Miller-Eshleman <micah.millereshleman@gmail.com>
2018-10-22 22:58:01 -0700

    Use routing-agnostic Google Console verification method

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,19 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link
             href="https://fonts.googleapis.com/css?family=Alegreya:700|Fira+Sans"
             rel="stylesheet"
             type="text/css"
+          />
+          <meta
+            name="google-site-verification"
+            content="taWifGRhg1p5PJ6vWZ8JgRQukYdxdTdfLc15zs23nqc"
           />
         </Head>
         <body>
